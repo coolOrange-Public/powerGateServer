a47af1d952d518e7eda5ab57ebcdbe79a013919b
coolOrange-Public <git@coolorange.com>
2017-01-18 10:28:26 +0100

    Updated ERP Plugin

---
+++ Plugins/ERP/Services/MaterialService/Entities/Material.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Data.Services.Common;
+
+namespace ErpServices.Services.MaterialService.Entities
+{
+	[DataServiceKey("Number")]
+	[DataServiceEntity]
+	public class Material
+	{
+		public string Number { get; set; }
+		public string UnitOfMeasure { get; set; }
+		public string Type { get; set; }
+		public IEnumerable<MaterialDescription> Descriptions { get; set; }
+
+		public Material()
+		{
+			Descriptions = new List<MaterialDescription>();
+		}
+	}
+}
