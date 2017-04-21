86d81026b24c750554fef43d868d7b8ad508b02b
coolOrange-Public <git@coolorange.com>
2017-04-21 15:11:05 +0200

    Updated SAP Plugin

---
+++ Plugins/SAP/SapServices.Tests/ContainerBasedTests.cs
@@ -0,0 +1,16 @@
+﻿using AutofacContrib.NSubstitute;
+using NUnit.Framework;
+
+namespace SapServices.Tests
+{
+	public class ContainerBaseTest
+	{
+		internal AutoSubstitute Container;
+
+		[SetUp]
+		public virtual void SetUpContainer()
+		{
+			Container =  new AutoSubstitute();
+		}
+	}
+}
---
+++ Plugins/SAP/Services/ContextEntitySetBase.cs
@@ -0,0 +1,17 @@
+﻿using SapServices.Database;
+
+namespace SapServices.Services
+{
+	public abstract class ContextEntitySetBase<T,TContext> : SapServiceMethodBase<T>
+	{
+		protected ContextEntitySetBase(IEntityStores entityStores)
+			: base(entityStores)
+		{
+		}
+
+		protected IEntityStore<TContext> ContextStore
+		{
+			get { return EntityStores.ResolveFor<TContext>(); }
+		}
+	}
+}
