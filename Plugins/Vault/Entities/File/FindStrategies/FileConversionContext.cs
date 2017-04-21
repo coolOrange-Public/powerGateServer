d8aded4b097897e5dfb44587804a535a1d6053d9
coolOrange-Public <git@coolorange.com>
2017-04-21 15:01:12 +0200

    Updated Vault Plugin

---
+++ Plugins/Vault/Entities/File/FindStrategies/FileConversionContext.cs
@@ -0,0 +1,11 @@
+﻿using coolOrange.Powershell.Cmdlets.Cmdlets.Vault.Facade;
+using VaultServices.Entities.Base;
+
+namespace VaultServices.Entities.File.FindStrategies
+{
+	public class FileConversionContext
+	{
+		 public IVaultEntityFactory EntityFactory { get; set; }
+		 public IAssociationService<File> AssociationService { get; set; }
+	}
+}
---
+++ Plugins/Vault/VaultServiceEnvironment.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using powerVault.Services;
+using powerVault.Services.Vault;
+using VaultServices.Environment;
+
+namespace VaultServices
+{
+	public class VaultServiceEnvironment
+	{
+		public static IVaultEnvironment Load()
+		{
+			var containerBuilder = new ContainerBuilder();
+			containerBuilder.RegisterType<SystemImpl>().As<ISystem>();
+			containerBuilder.RegisterType<AssemblyResolver>().As<IAssemblyResolver>();
+			containerBuilder.RegisterType<PowerVaultAssemblyLoader>().As<IAssemblyLoader>();
+			containerBuilder.RegisterType<VaultAssemblyLoader>().As<IAssemblyLoader>();
+			containerBuilder.RegisterType<PowerVaultEnvironment>().As<IVaultEnvironment>();
+			containerBuilder.RegisterType<PowerVaultEnvironment>().As<IVaultEnvironment>();
+			var container = containerBuilder.Build();
+			var vaultEnvironment = container.Resolve<IVaultEnvironment>();
+			return vaultEnvironment;
+		}
+	}
+}
