c0d67ae931c2f20ec25701bcde04dce172c2aa36
coolOrange-Public <git@coolorange.com>
2018-03-27 12:33:26 +0200

    Updated CatalogService Plugin

---
+++ Plugins/CatalogService/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CatalogService")]
+[assembly: AssemblyDescription("")]
+
+[assembly: Guid("d1de2425-26f9-4742-953c-99292c6c9661")]
