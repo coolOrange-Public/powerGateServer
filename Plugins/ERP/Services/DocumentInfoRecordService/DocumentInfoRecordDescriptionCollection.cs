3b995c844cdd1901fd8af3d744844e42570311de
coolOrange-Public <git@coolorange.com>
2019-05-21 17:35:07 +0200

    Updated ERP Plugin

---
+++ Plugins/ERP/Services/DocumentInfoRecordService/DocumentInfoRecordDescriptionCollection.cs
@@ -0,0 +1,19 @@
+﻿using ErpServices.Database;
+using ErpServices.Services.DocumentInfoRecordService.Entities;
+
+namespace ErpServices.Services.DocumentInfoRecordService
+{
+	public class DocumentInfoRecordDescriptionCollection :
+		DirNavigationPropertyCollectionEntitySet<DocumentInfoRecordDescription>
+	{
+		public DocumentInfoRecordDescriptionCollection(IEntityStores entityStores) 
+			: base(entityStores)
+		{
+		}
+
+		public override string Name
+		{
+			get { return "DocumentInfoRecordDescriptionCollection"; }
+		}
+	}
+}
