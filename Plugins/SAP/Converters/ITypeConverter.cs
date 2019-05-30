373fe8fd71939a6314aa7d0528754b8190e07e78
coolOrange-Public <git@coolorange.com>
2019-05-30 11:12:25 +0200

    Updated SAP Plugin

---
+++ Plugins/SAP/Converters/ITypeConverter.cs
@@ -0,0 +1,8 @@
+namespace SapServices.Converters
+{
+	public interface ITypeConverter<TFrom, TTo>
+	{
+		TTo ConvertTo(TFrom from);
+		TFrom ConvertFrom(TTo to);
+	}
+}
---
+++ Plugins/SAP/Services/DocumentInfoRecordService/DocumentInfoRecordDataCollection.cs
@@ -0,0 +1,18 @@
+﻿using SapServices.Database;
+using SapServices.Services.DocumentInfoRecordService.Entities;
+
+namespace SapServices.Services.DocumentInfoRecordService
+{
+	public class DocumentInfoRecordDataCollection : DirNavigationPropertyEntitySet<DocumentInfoRecordData>
+	{
+		public DocumentInfoRecordDataCollection(IEntityStores entityStores)
+			: base(entityStores)
+		{
+		}
+
+		public override string Name
+		{
+			get { return "DocumentInfoRecordDataCollection"; }
+		}
+	}
+}
