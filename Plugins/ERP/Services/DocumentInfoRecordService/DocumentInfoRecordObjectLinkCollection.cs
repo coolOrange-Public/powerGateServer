d6b34c931d555eb7b928d812b47c758f520e5e4b
coolOrange-Public <git@coolorange.com>
2019-05-30 11:10:57 +0200

    Updated ERP Plugin

---
+++ Plugins/ERP/Services/DocumentInfoRecordService/DocumentInfoRecordObjectLinkCollection.cs
@@ -0,0 +1,19 @@
+﻿using ErpServices.Database;
+using ErpServices.Services.DocumentInfoRecordService.Entities;
+
+namespace ErpServices.Services.DocumentInfoRecordService
+{
+	public class DocumentInfoRecordObjectLinkCollection : DirNavigationPropertyCollectionEntitySet<DocumentInfoRecordObjectLink>
+	{
+		public override string Name
+		{
+			get { return "DocumentInfoRecordObjectLinkCollection"; }
+		}
+
+
+		public DocumentInfoRecordObjectLinkCollection(IEntityStores entityStores)
+			: base(entityStores)
+		{
+		}
+	}
+}
---
+++ Plugins/ERP/Services/DocumentInfoRecordService/Entities/CharacteristicValues.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Services.Common;
+
+namespace ErpServices.Services.DocumentInfoRecordService.Entities
+{
+	[DataServiceKey("Documentversion", "Documenttype", "Documentpart", "Documentnumber", "ClassType", "CharName", "CharValue")]
+	[DataServiceEntity]
+	public class CharacteristicValues
+	{
+		public string Documentversion { get; set; }
+		public string Documenttype { get; set; }
+		public string Documentpart { get; set; }
+		public string Documentnumber { get; set; }
+
+		public string ClassType { get; set; }
+		public string ClassName { get; set; }
+		public string CharName { get; set; }
+		public string CharValue { get; set; }
+	}
+}
