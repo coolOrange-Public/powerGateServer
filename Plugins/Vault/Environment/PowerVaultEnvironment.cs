e315fd90372ce124914bcb6aaa15e65fdcc7dd41
coolOrange-Public <git@coolorange.com>
2019-09-11 15:17:09 +0200

    Updated Vault Plugin

---
+++ Plugins/Vault/Environment/PowerVaultEnvironment.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using powerVault.Cmdlets.Cmdlets.Vault.Facade;
+using coolOrange.VaultServices.Vault;
+using IVaultConnection = coolOrange.VaultServices.Vault.IVaultConnection;
+
+namespace VaultServices.Environment
+{
+	public class PowerVaultEnvironment : IVaultEnvironment
+	{
+		internal readonly VaultEnvironment Environment;
+
+		public PowerVaultEnvironment(IEnumerable<IAssemblyLoader> assemblyLoaders)
+		{
+			assemblyLoaders.ToList().ForEach(loader => loader.AddResolver());
+			Environment = new VaultEnvironment();
+		}
+
+		public IVaultConnection Login(VaultLoginCredentials loginCredentials)
+		{
+			Autodesk.DataManagement.Client.Framework.Library.Initialize(false);
+			return Environment.Login(loginCredentials);
+		}
+
+		internal class VaultEnvironment : IVaultEnvironment
+		{
+			powerVault.Cmdlets.Cmdlets.Vault.Facade.IVaultConnection _vault;
+			internal powerVault.Cmdlets.Cmdlets.Vault.Facade.IVaultConnection Vault
+			{
+				get { return _vault ?? (_vault = new VaultPowerShellProxy()); }
+				set { _vault = value; }
+			}
+
+			public IVaultConnection Login(VaultLoginCredentials loginCredentials)
+			{
+				Vault.Login(loginCredentials.User, loginCredentials.Password, loginCredentials.Server, loginCredentials.Vault);
+				return new ExistingVaultConnection(Vault.Connection);
+			}
+		}
+	}
+}
---
+++ Plugins/Vault/VaultServices.Tests/ContainerBasedTests.cs
@@ -0,0 +1,16 @@
+﻿using AutofacContrib.NSubstitute;
+using NUnit.Framework;
+
+namespace VaultServices.Tests
+{
+	public class ContainerBasedTests
+	{
+		internal AutoSubstitute Container;
+
+		[SetUp]
+		public virtual void SetUpContainer()
+		{
+			Container = new AutoSubstitute();
+		}
+	}
+}
