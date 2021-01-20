6006bf15e2bea002169f9b0883227c863677a2a5
coolOrange-Public <git@coolorange.com>
2021-01-20 15:09:35 +0100

    Updated ERP Plugin

---
+++ Plugins/ERP/Services/MaterialService/MaterialService.cs
@@ -0,0 +1,28 @@
+﻿using ErpServices.Services.MaterialService.Entities;
+using powerGateServer.SDK;
+
+namespace ErpServices.Services.MaterialService
+{
+	[WebServiceData("PGS/ERP", "MaterialService")]
+	public class MaterialService : ErpServiceBase
+	{
+		protected override void CreateTablesForEntities()
+		{
+			EntityStores.AddStoreFor<MaterialDescription>();
+			EntityStores.AddStoreFor<Material>();
+		}
+
+		protected override void RegisterLookups()
+		{
+			AddLookupFor<MaterialTypeLookup>();
+			AddLookupFor<UomLookup>();
+			AddLookupFor<LanguageLookup>();
+		}
+
+		protected override void RegisterEntitySets()
+		{
+			AddEntitySetOfType<Materials>();
+			AddEntitySetOfType<MaterialDescriptions>();
+		}
+	}
+}
