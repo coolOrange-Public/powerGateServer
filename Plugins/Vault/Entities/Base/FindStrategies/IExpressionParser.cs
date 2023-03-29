c46bf78acfce9ac85674fea6ac1654892fba9ee3
viktorgobbi <37219397+viktorgobbi@users.noreply.github.com>
2023-03-29 15:43:15 +0200

    Removed Vault plugin, no longer maintained and supported
    -> Removed Plugins/Vault as the plugin is no longer supported and maintained

--- Plugins/Vault/Entities/Base/FindStrategies/IExpressionParser.cs
+++
@@ -1,9 +0,0 @@
-﻿using powerGateServer.SDK;
-
-namespace VaultServices.Entities.Base.FindStrategies
-{
-	public interface IExpressionParser<TFrom>
-	{
-		IExpression<T> ParseFor<T>(IExpression<TFrom> propertyExpression) where T : IBaseObject;
-	}
-}
--- Plugins/Vault/Entities/Property/Properties.cs
+++
@@ -1,8 +0,0 @@
-﻿using VaultServices.Entities.Base;
-
-namespace VaultServices.Entities.Property
-{
-	public class Properties : EntityOperationBase<Property>
-    {
-    }
-}
