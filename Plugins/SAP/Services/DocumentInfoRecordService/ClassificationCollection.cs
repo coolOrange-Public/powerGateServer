a6c6d24de1386e7be999ea477745f1546ebfcf1b
coolOrange-Public <git@coolorange.com>
2023-03-29 15:39:31 +0200

    Updated SAP Plugin

---
+++ Plugins/SAP/Services/DocumentInfoRecordService/ClassificationCollection.cs
@@ -0,0 +1,18 @@
+﻿using SapServices.Database;
+using SapServices.Services.DocumentInfoRecordService.Entities;
+
+namespace SapServices.Services.DocumentInfoRecordService
+{
+	public class ClassificationCollection : DirNavigationPropertyCollectionEntitySet<Classification>
+	{
+		public ClassificationCollection(IEntityStores entityStores)
+			: base(entityStores)
+		{
+		}
+
+		public override string Name
+		{
+			get { return "ClassificationCollection"; }
+		}
+	}
+}
