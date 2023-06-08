d4e50461e971a0fa38c1a89f2d1e9b9b602be33f
coolOrange-Public <git@coolorange.com>
2023-06-08 16:29:51 +0200

    Updated TrafficLogging Behavior

---
+++ Behaviors/TrafficLogging/TrafficLoggingBehaviorExtensionElement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ServiceModel.Configuration;
+
+namespace TrafficLogging
+{
+	public class TrafficLoggingBehaviorExtensionElement : BehaviorExtensionElement
+	{
+		public override Type BehaviorType
+		{
+			get { return typeof(TrafficLoggingBehavior); }
+		}
+
+		protected override object CreateBehavior()
+		{
+			return new TrafficLoggingBehavior();
+		}
+	}
+}
