9cdef6dff565fcb8902fca0006be2beccb48d099
Products Team User <products@coolorange.com>
2025-01-21 14:21:51 +0100

    Updated SAP Plugin

---
+++ Plugins/SAP/Services/DocumentInfoRecordService/DocumentInfoRecordDescriptionCollection.cs
@@ -0,0 +1,19 @@
+﻿using SapServices.Database;
+using SapServices.Services.DocumentInfoRecordService.Entities;
+
+namespace SapServices.Services.DocumentInfoRecordService
+{
+	public class DocumentInfoRecordDescriptionCollection :
+		DirNavigationPropertyCollectionEntitySet<DocumentInfoRecordDescription>
+	{
+		public DocumentInfoRecordDescriptionCollection(IEntityStores entityStores) 
+			: base(entityStores)
+		{
+		}
+
+		public override string Name
+		{
+			get { return "DocumentInfoRecordDescriptionCollection"; }
+		}
+	}
+}
