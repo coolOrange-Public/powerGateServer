71919f580a79a018c884d1f6537d6fb251e24bdd
Products Team User <products@coolorange.com>
2025-01-21 14:21:43 +0100

    Updated CatalogService Plugin

---
+++ Plugins/CatalogService/CatalogService.cs
@@ -0,0 +1,13 @@
+﻿using powerGateServer.SDK;
+
+namespace CatalogService
+{
+	[WebServiceData("PGS", "CATALOGSERVICE")]
+	public class CatalogService : WebService
+	{
+		public CatalogService()
+		{
+			AddMethod(new ServiceCollection(this));
+		}
+	}
+}
