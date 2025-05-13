8aead00d18d59693aaac605455e1af469794204e
Products Team User <products@coolorange.com>
2025-05-13 15:31:01 +0200

    Updated CatalogService Plugin

---
+++ Plugins/CatalogService/CatalogService.Tests/ContainerBasedTests.cs
@@ -0,0 +1,16 @@
+﻿using AutofacContrib.NSubstitute;
+using NUnit.Framework;
+
+namespace CatalogService.Tests
+{
+	public class ContainerBaseTest
+	{
+		internal AutoSubstitute Container;
+
+		[SetUp]
+		public virtual void SetUpContainer()
+		{
+			Container =  new AutoSubstitute();
+		}
+	}
+}
