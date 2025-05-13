11c752de5c9851924aa3c03d7e5addbf006bf7f9
Products Team User <products@coolorange.com>
2025-05-13 15:31:09 +0200

    Updated ERP Plugin

---
+++ Plugins/ERP/ErpServices.Tests/ErpServiceMethodBaseTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Data.Services.Common;
+using ErpServices.Database;
+using ErpServices.Services;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace ErpServices.Tests
+{
+	[TestFixture]
+	public class ErpServiceMethodBaseTests
+	{
+		[Test]
+		public void ClientId_returns_empty_when_client_is_not_authenticated()
+		{
+			var requestClientId = Substitute.For<Func<string>>();
+			requestClientId.Invoke().ReturnsForAnyArgs("");
+			
+			var erpServiceBase = new TestsErpServiceMethodBaseImpl();
+			erpServiceBase.GetCurrentClientId = requestClientId;
+			var clientId = erpServiceBase.GetErpServiceMethodBaseClientId();
+
+			Assert.AreEqual(string.Empty, clientId);
+		}
+
+		[Test]
+		public void ClientId_returns_username_of_authenticated_client()
+		{
+			var requestClientId = Substitute.For<Func<string>>();
+			requestClientId.Invoke().ReturnsForAnyArgs("hahahahahHAHAHAHA");
+			
+			var erpServiceBase = new TestsErpServiceMethodBaseImpl();
+			erpServiceBase.GetCurrentClientId = requestClientId;
+			var clientId = erpServiceBase.GetErpServiceMethodBaseClientId();
+
+			Assert.AreEqual("hahahahahHAHAHAHA", clientId);
+		}
+
+		[DataServiceKey("Id")]
+		class TestEntity
+		{
+			public string Name { get; set; }
+			public int Id { get; set; }
+		}
+
+		class TestsErpServiceMethodBaseImpl : ErpServiceMethodBase<TestEntity>
+		{
+			public TestsErpServiceMethodBaseImpl() : base(Substitute.For<IEntityStores>())
+			{
+			}
+
+			public string GetErpServiceMethodBaseClientId()
+			{
+				return ClientId;
+			}
+		}
+	}
+}
---
+++ Plugins/ERP/ErpServices.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ErpServices.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ErpServices.Tests")]
+[assembly: AssemblyCopyright("© 2014 coolOrange s.r.l.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c945bd0e-5cf5-448a-b802-6135c6af5814")]
