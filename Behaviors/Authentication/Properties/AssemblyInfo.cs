933e1620522961d5e78ca720ac0d67ac99b28a05
Products Team User <products@coolorange.com>
2026-01-19 16:41:40 +0100

    Updated Authentication Behavior

---
+++ Behaviors/Authentication/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Authentication Sample")]
+[assembly: AssemblyDescription("Authentication sample for powerGateServer")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("coolOrange s.r.l.")]
+[assembly: AssemblyProduct("powerGateServer authentication sample")]
+[assembly: AssemblyCopyright("© 2025 coolOrange s.r.l.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("62a491fa-a820-4c1f-91d2-84c7da638123")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
